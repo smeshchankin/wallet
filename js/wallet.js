913df7f6e53bcc4b462ac3c07a096e2433f5a633
Sergey Meshchankin <smeshchankin@gmail.com>
2020-04-26 02:16:53 +0300

    [WO-002] Logic: find all manipulate elements

---
+++ js/wallet.js
@@ -0,0 +1,9 @@
+(function() {
+    const balanceElem = document.getElementById('balance');
+    const incomeElem = document.getElementById('income');
+    const expensesElem = document.getElementById('expenses');
+    const historyElem = document.getElementById('history');
+    const formElem = document.getElementById('form');
+    const operationElem = document.getElementById('operation');
+    const amountElem = document.getElementById('amount');
+})();
