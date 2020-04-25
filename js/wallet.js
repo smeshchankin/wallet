2032de5979a67f510caaec8eed8ca736552f1541
Sergey Meshchankin <smeshchankin@gmail.com>
2020-04-26 02:17:51 +0300

    [WO-002] Logic: history example

--- js/wallet.js
+++ js/wallet.js
@@ -6,4 +6,12 @@
     const formElem = document.getElementById('form');
     const operationElem = document.getElementById('operation');
     const amountElem = document.getElementById('amount');
+
+    const dbHistory = [
+        {id: 1, desc: 'Salary', amount: 300},
+        {id: 2, desc: 'Food', amount: -80},
+        {id: 3, desc: 'Cinema', amount: -50},
+        {id: 4, desc: 'Freelance', amount: 100},
+        {id: 5, desc: 'Books', amount: -40}
+    ];
 })();
